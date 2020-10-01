d63ce6bcb7bdd4a4601f0917b561bb4e02270927
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-10-01 19:59:47 -0400

    update with jwt

---
+++ build/precache-manifest.3eebf8a7192bd7f6f8b6e2c109f8975f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde7d06aa0b2d42d5967a0207bf65ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb3a4ea6e673b0a327a",
+    "url": "/static/css/main.b235c41d.chunk.css"
+  },
+  {
+    "revision": "521413cad3810696a4c8",
+    "url": "/static/js/2.7ec9ec36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ec9ec36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb3a4ea6e673b0a327a",
+    "url": "/static/js/main.0aa9184e.chunk.js"
+  },
+  {
+    "revision": "2d93ddce714052c0bbe7",
+    "url": "/static/js/runtime-main.e6b6e052.js"
+  }
+]);
